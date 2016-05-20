d98cc49d046135f065aef1ad45d574d8d25b5208
Ravi Kumar <ravikumarmvs@gmail.com>
2016-05-20 18:10:42 +0530

    final

--- AngularTraining/AngularTraining/Controllers/employeesController.js
+++ AngularTraining/AngularTraining/Controllers/employeesController.js
@@ -33,8 +33,6 @@ angular.module('EmployeeDB').controller('EmployeesEntryCtrl', function ($rootSco
 
         employeeFactory.AddArrayValue(newEmployee);
         employeeFactory.UpdateEmployeeStatusString({ statusValue: 'status: Employee Added', empId: newEmployee.id });
-
-        //$rootScope.rootHistoryItems.push({ name: newEmployee.name + " has been added", indexValue: Math.random() });
         updateActivities({ value: newEmployee.name + " has been added", junk: Math.random() });
         init();
     }
@@ -43,12 +41,10 @@ angular.module('EmployeeDB').controller('EmployeesEntryCtrl', function ($rootSco
 
         employeeFactory.EmployeeArray.splice(employeeFactory.EmployeeArray.indexOf(e), 1);
         employeeFactory.UpdateEmployeeStatusString({ statusValue: 'status: Employee Deleted', empId: e.id });
-        //$rootScope.rootHistoryItems.push({ name: e.name + " has been removed", indexValue: e.id });
         updateActivities({value: e.name + " has been removed", junk: Math.random() });
     }
     
     $scope.viewProfile = function (currentEmployee) {
-        //$rootScope.rootHistoryItems.push({ name: currentEmployee.name + " profile viewed", indexValue: currentEmployee.id });
         updateActivities({ value: currentEmployee.name + " profile viewed", junk: Math.random() });
         employeeFactory.DisableEmployeeProfileUpdationButton(false);
         $state.go('profile', {id:currentEmployee.id});
--- AngularTraining/AngularTraining/Controllers/profileController.js
+++ AngularTraining/AngularTraining/Controllers/profileController.js
@@ -4,8 +4,7 @@ angular.module('ProfileDB').controller('EmployeeProfileCtrl', function ($rootSco
     $rootScope.isLoginPage = false;
     $scope.currentEmployee = employeeFactory.GetEmployeeById($stateParams.id);
     $scope.copiedEmployee = angular.copy($scope.currentEmployee);
-    $scope.IsDisableUpdateButton = true;
-    //var loadedEmployee = { name: $scope.currentEmployee.name, role: $scope.currentEmployee.role, projectDetails: { project: $scope.currentEmployee.projectDetails.project, projectLocation: $scope.currentEmployee.projectDetails.projectLocation }, index: $scope.currentEmployee.index };    
+    $scope.IsDisableUpdateButton = true;    
     $scope.DisableUpdation = function () {        
         return $scope.IsDisableUpdateButton;
     }
@@ -21,8 +20,6 @@ angular.module('ProfileDB').controller('EmployeeProfileCtrl', function ($rootSco
 
     $scope.updateEmployees = function (currentEmployee) {        
         var newEmployee = { name: currentEmployee.name, role: currentEmployee.role, projectDetails: { project: currentEmployee.projectDetails.project, projectLocation: currentEmployee.projectDetails.projectLocation }};
-
-        //$rootScope.rootHistoryItems.push({ name: currentEmployee.name + " profile changed", indexValue: currentEmployee.id });
         updateActivities({ value: currentEmployee.name + " profile changed", junk: Math.random() });
         employeeFactory.UpdateEmployeeValue(newEmployee, currentEmployee.id);
         employeeFactory.UpdateEmployeeStatusString({ statusValue: 'status: Employee Updated', empId: currentEmployee.id });
